e9b625971e90e8efe033be1ffc1ab6b9ef6cc9dc
Raj Kumar <r.rajkumar1514@gmail.com>
2026-04-28 10:50:17 +0530

    Add test data

--- utils/ExcelReader.ts
+++ utils/ExcelReader.ts
@@ -1,12 +1,11 @@
 import * as XLSX from 'xlsx';
 import * as path from 'path';
-import * as os from 'os';
 
 export class ExcelReader {
 
   static readExcel(fileName: string, sheetIndex = 0) {
 
-    const filePath = path.join(os.homedir(), 'Desktop', fileName);
+    const filePath = path.resolve(process.cwd(), 'test-data', fileName);
 
     const workbook = XLSX.readFile(filePath);
 
